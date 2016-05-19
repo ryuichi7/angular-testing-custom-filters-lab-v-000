4df1d8ab84e7833c0b7bf12415027fa8991cbe10
Brett <brettrheenan@gmail.com>
2016-05-19 10:58:38 -0700

    Done.

--- tests/FavoriteFood.spec.js
+++ tests/FavoriteFood.spec.js
@@ -1,11 +1,30 @@
 describe('Favorite Food Filter', function () {
 	var $controller;
-
+			
 	beforeEach(module('app'));
 
 	beforeEach(inject(function ($injector) {
 		$filter = $injector.get('$filter');
 	}));
 
+
+	it('filters by favorite food', function() {
+		var mockedList = [{
+			name: "Brett",
+			favoriteFood: "Chicken"
+		}, {
+			name: "Evan",
+			favoriteFood: "Cheese"
+		}, {
+			name: "Emily",
+			favoriteFood: "Cheese"
+		}];
+
+		var foodFilter = $filter('favoriteFood')(mockedList, 'Cheese')
+
+		expect(foodFilter.length).toBe(2)
+		expect(foodFilter[0].name).toBe('Evan')
+
+	})
 	
 });
--- tests/removeAllVowels.spec.js
+++ tests/removeAllVowels.spec.js
@@ -7,4 +7,12 @@ describe('removeAllVowels Filter', function () {
 		$filter = $injector.get('$filter');
 	}));
 
+	it('removes all vowels from a string', function() {
+		var str = "Hi There How are you!"
+
+		var vowelFilter = $filter('removeAllVowels')(str)
+
+		expect(vowelFilter).toBe('H Thr Hw r y!')
+
+	})
 });
